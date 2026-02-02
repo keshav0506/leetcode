8460b014c8059e6669cf8921cfaa7fd07ccd2ba9
keshav0506 <keshavkhandelwal300@gmail.com>
2026-02-02 20:55:43 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,24 @@
+// Last updated: 02/02/2026, 20:55:43
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        int mid = s+(e-s)/2;
+        int ans = 0;
+        while(s<=e){
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                e = mid-1;
+                ans = mid;
+            }
+            else{
+                s= mid +1;
+            }
+            mid = s+(e-s)/2;
+        }
+        return -1;
+    }
+};
